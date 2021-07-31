0b8a7ce5037618a494fb52bec022c82ef60bddac
Longtran10 <88227897+Longtran10@users.noreply.github.com>
2021-07-31 15:40:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,18 @@ const CONFIG = {
     introTitle: 'Mình bảo cái lày!',
     introDesc: `Chưa bao giờ anh nghĩ
     rằng em là gu của anh đâu
+    
     Chưa bao giờ anh nghĩ
     rằng anh, lại Fall in love with U`,
     btnIntro: 'hihi',
     title: 'I go insane every time em bên mình 😙',
-    desc: 'i will keep u safe inside my angel wings ',
+    desc: 'Chúng mình phải luôn yêu thương nhau nhá ',
     btnYes: 'Vẫn cứ là yêu LG 🙂',
-    btnNo: 'Không yêu mình à 🙂',
-    question:'Tại sao mình lại quen nhau nhỉ???',
+    btnNo: 'Không nha 🙂',
+    question:'Hmmm...tại sao mình lại quen nhau nhỉ???',
     btnReply: 'Gửi cho LG 😒',
     reply: 'Vì LG đẹp trai nè, vui tính nè 😳',
-    mess: 'Mình biết mà hehe!!!.Yêu PTH nhiều 😘😘',
+    mess: 'Mình biết mà, yêu PTH nhiều 😘😘',
     messDesc: 'Mau khỏe còn cãi nhau với mình nha 😛.',
     btnAccept: 'Vâng ạ, yêu LG 😎',
     messLink: 'https://www.facebook.com/long.gia.1020/' //link mess của các bạn. VD: https://m.me/nam.nodemy
